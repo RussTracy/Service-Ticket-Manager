0d2b50953052d24186e402e10e1f87438ff1890b
Matthew Damron <mattdamron@msn.com>
2021-02-26 15:53:22 -0700

    updated colors

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,5 +8,8 @@ module.exports = {
     let timeLaps = Math.floor(duration.asHours()) + 'h';
 
     return `${timeLaps}`;
-  }
+  },
+  ifEquals: function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+  } 
 };
