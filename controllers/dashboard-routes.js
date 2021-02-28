7fcedff1e9a0d5256ca938cb6d8dff99a0748280
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2021-02-27 22:15:24 -0700

    added parts of the dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,8 +4,41 @@ const withAuth = require('../utils/auth');
 
 // get all posts for dashboard
 router.get('/', withAuth, (req, res) => {
-  // res.render('dashboard', { tickets, loggedIn: true });
-  res.render('dashboard', { loggedIn: true });
+  Ticket.findAll({
+    attributes: [
+      'id',
+      'title',
+      'description',
+      'user_id',
+      'department_id',
+      'status_id',
+      'created_at'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      },
+      {
+        model: Status,
+        attributes: ['status_type']
+      },
+      {
+        model: Priority,
+        attributes: ['priority']
+      },
+      {
+        model: Department,
+        attributes: ['department_name']
+      }],
+      where:{user_id:req.session.user_id}
+  }).then(dbTicketData => {
+    const tickets = dbTicketData.map(ticket => ticket.get({ plain: true }));
+
+     res.render('dashboard', { tickets, loggedIn: true });
+    
+  })
 });
+
 
 module.exports = router;
