165d23e9f0d92fa7bc3564f776294254ec9b393c
Russ <ocjade@comcast.net>
2021-03-04 13:56:59 -0700

    Dynamic Form Select Options

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -31,16 +31,34 @@ router.get('/', withAuth, (req, res) => {
         model: Department,
         attributes: ['department_name', 'id']
       }],
-      where:{user_id:req.session.user_id}
+    where: { user_id: req.session.user_id }
   })
     .then(dbTicketData => {
       const tickets = dbTicketData.map(ticket => ticket.get({ plain: true }));
 
-      res.render('dashboard', { tickets, loggedIn: req.session.loggedIn, userName: req.session.username, dashboardCard: true });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+      Priority.findAll({
+        attributes: [
+          'id',
+          'priority'
+        ]
+      }).then(dbPriorityData => {
+        const priorities = dbPriorityData.map(priority => priority.get({ plain: true }));
+
+        Department.findAll({
+          attributes: [
+            'id',
+            'department_name'
+          ]
+        }).then(dbDepartmentData => {
+          const departments = dbDepartmentData.map(department => department.get({ plain: true }));
+
+          res.render('dashboard', { tickets, priorities, departments, loggedIn: req.session.loggedIn, userName: req.session.username, dashboardCard: true });
+        })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      });
     });
 });
 
